95b6591de6978805f2f04e1de34efc8e23424bb5
Erick Salas-Chaverri <ericksc@gmail.com>
2019-03-30 12:13:16 -0600

    Add files via upload
    Estos son los primos archicos del curso de python basico

---
+++ semana1/practica2.py
@@ -0,0 +1,14 @@
+# Esta es la practica para escribir 2 lineas
+# en consola
+
+'''
+Comentario en multiples lineas
+util para explicar mas detalles
+'''
+
+print('Hola a todos!')
+print('buenos dias')
+
+# varias lineas en un solo print
+print('mi primera linea', 'mi segunda linea')
+print('hola de nuevo')
---
+++ semana1/practica3.py
@@ -0,0 +1,17 @@
+# para revisar los tipos de datos
+
+# numero entero
+# definir un nuemro entero
+
+123456789
+
+mi_variable = 13456789
+
+# caso 1
+print('El valor de la variable llamada "mi variable es"', mi_variable)
+
+# caso 2
+print("El valor de la variable llamada 'mi variable es'", mi_variable)
+
+# caso 3
+print("El valor de la variable llamada \"mi variable es\"", mi_variable)
